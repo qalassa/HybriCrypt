c9ae2e73ff01c38933ae6c5b401ec8f4fb810f8e
qalassa <109701506+qalassa@users.noreply.github.com>
2023-05-08 16:18:25 +0300

    Update cryptolib.cpp

--- src/cryptolib.cpp
+++ src/cryptolib.cpp
@@ -1,86 +1,91 @@
+#include <iostream>
 #include "cryptolib.h"
 
 using namespace std;
 using namespace CryptoPP;
 
-void GenerateRSAKeyPair(int keyLength, string &privateKey, string &publicKey) {
+void print_menu() {
+    cout << "Choose an option:" << endl;
+    cout << "1. Generate RSA key pair" << endl;
+    cout << "2. Diffie-Hellman Key Exchange" << endl;
+    cout << "3. Encrypt message using AES-CBC" << endl;
+    cout << "4. Decrypt message using AES-CBC" << endl;
+    cout << "5. Generate SHA-256 hash" << endl;
+    cout << "6. Exit" << endl;
+}
+
+int main() {
+    int choice;
     AutoSeededRandomPool rng;
+    SecByteBlock sharedKey;
+    CryptoPP::byte iv[AES::BLOCKSIZE];
+    rng.GenerateBlock(iv, sizeof(iv));
 
-    RSA::PrivateKey rsaPrivate;
-    rsaPrivate.GenerateRandomWithKeySize(rng, keyLength);
+    do {
+        print_menu();
+        cin >> choice;
+        cin.ignore();
 
-    RSA::PublicKey rsaPublic(rsaPrivate);
+        switch (choice) {
+            case 1: {
+                string privateKey, publicKey;
+                GenerateRSAKeyPair(2048, privateKey, publicKey);
+                cout << "Private key: " << privateKey << endl;
+                cout << "Public key: " << publicKey << endl;
+                break;
+            }
+            case 2: {
+                DiffieHellmanKeyExchange(rng, sharedKey);
+                cout << "Shared key: ";
+                for (size_t i = 0; i < sharedKey.size(); i++) {
+                    cout << hex << (int)sharedKey[i];
+                }
+                cout << endl;
+                break;
+            }
+            case 3: {
+                if (sharedKey.empty()) {
+                    cout << "Please perform Diffie-Hellman key exchange first." << endl;
+                    break;
+                }
+                cout << "Enter the message to encrypt: ";
+                string plainText;
+                getline(cin, plainText);
+                CryptoPP::byte iv[AES::BLOCKSIZE];
+                rng.GenerateBlock(iv, sizeof(iv));
+                string cipherText = EncryptAES_CBC(plainText, sharedKey, iv);
+                cout << "Encrypted message: " << cipherText << endl;
+                break;
+            }
+            case 4: {
+                if (sharedKey.empty()) {
+                    cout << "Please perform Diffie-Hellman key exchange first." << endl;
+                    break;
+                }
+                cout << "Enter the message to decrypt: ";
+                string cipherText;
+                getline(cin, cipherText);
+                CryptoPP::byte iv[AES::BLOCKSIZE];
+                rng.GenerateBlock(iv, sizeof(iv));
+                string decryptedText = DecryptAES_CBC(cipherText, sharedKey, iv);
+                cout << "Decrypted message: " << decryptedText << endl;
+                break;
+            }
+            case 5: {
+                cout << "Enter the message to hash: ";
+                string message;
+                getline(cin, message);
+                string hash = GenerateSHA256Hash(message);
+                cout << "SHA-256 hash: " << hash << endl;
+                break;
+            }
+            case 6:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+        }
+    } while (choice != 6);
 
-    HexEncoder privKeyEncoder(new StringSink(privateKey));
-    rsaPrivate.Save(privKeyEncoder);
-    privKeyEncoder.MessageEnd();
-
-    HexEncoder pubKeyEncoder(new StringSink(publicKey));
-    rsaPublic.Save(pubKeyEncoder);
-    pubKeyEncoder.MessageEnd();
-}
-
-string EncryptAES_CBC(const string &plainText, const SecByteBlock &key, const byte *iv) {
-    string cipherText;
-
-    CBC_Mode<AES>::Encryption encryptor;
-    encryptor.SetKeyWithIV(key, key.size(), iv);
-
-    StreamTransformationFilter stf(encryptor, new StringSink(cipherText));
-    stf.Put(reinterpret_cast<const byte *>(plainText.data()), plainText.size());
-    stf.MessageEnd();
-
-    return cipherText;
-}
-
-string DecryptAES_CBC(const string &cipherText, const SecByteBlock &key, const byte *iv) {
-    string decryptedText;
-
-    CBC_Mode<AES>::Decryption decryptor;
-    decryptor.SetKeyWithIV(key, key.size(), iv);
-
-    StreamTransformationFilter stf(decryptor, new StringSink(decryptedText));
-    stf.Put(reinterpret_cast<const byte *>(cipherText.data()), cipherText.size());
-    stf.MessageEnd();
-
-    return decryptedText;
-}
-
-void SimulatePublicKeyExchange(const SecByteBlock &publicKeyToSend, SecByteBlock &publicKeyToReceive) {
-    publicKeyToReceive.Assign(publicKeyToSend.data(), publicKeyToSend.size());
-}
-
-void DiffieHellmanKeyExchange(AutoSeededRandomPool &rng, SecByteBlock &sharedKey) {
-    DH dh;
-    dh.AccessGroupParameters().GenerateRandomWithKeySize(rng, 2048);
-
-    SecByteBlock privateKey(dh.PrivateKeyLength());
-    SecByteBlock publicKey(dh.PublicKeyLength());
-
-    dh.GenerateKeyPair(rng, privateKey, publicKey);
-
-    // Simulate the exchange of public keys with the other party
-    SecByteBlock otherPartyPrivateKey(dh.PrivateKeyLength());
-    SecByteBlock otherPartyPublicKey(dh.PublicKeyLength());
-    dh.GenerateKeyPair(rng, otherPartyPrivateKey, otherPartyPublicKey);
-
-    // Assume both parties call SimulatePublicKeyExchange
-    SecByteBlock exchangedPublicKey(dh.PublicKeyLength());
-    SimulatePublicKeyExchange(publicKey, exchangedPublicKey);
-
-    sharedKey.resize(dh.AgreedValueLength());
-    if (!dh.Agree(sharedKey, privateKey, exchangedPublicKey)) {
-        throw runtime_error("Failed to reach a shared secret.");
-    }
-}
-
-//-------------------------------------------------------------------
-
-string GenerateSHA256Hash(const string &message) {
-    SHA256 hash;
-    string digest;
-
-    StringSource(message, true, new HashFilter(hash, new HexEncoder(new StringSink(digest))));
-    
-    return digest;
+    return 0;
 }
